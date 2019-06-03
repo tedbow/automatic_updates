f1f29b265795b8046bf0b231c5a69bdafb6735ee
lucashedding <lucashedding@1463982.no-reply.drupal.org>
2019-06-03 15:42:24 -0500

    Issue #3053712 by heddn, eiriksm, catch, mbaynton: Provide context of how checker service is run

---
+++ src/ReadinessChecker/PhpSapi.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\automatic_updates\ReadinessChecker;
+
+use Drupal\Core\State\StateInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * Warn if PHP SAPI changes between checker executions.
+ */
+class PhpSapi implements ReadinessCheckerInterface {
+  use StringTranslationTrait;
+
+  /**
+   * The state service.
+   *
+   * @var \Drupal\Core\State\StateInterface
+   */
+  protected $state;
+
+  /**
+   * PhpSapi constructor.
+   *
+   * @param \Drupal\Core\State\StateInterface $state
+   *   The state service.
+   */
+  public function __construct(StateInterface $state) {
+    $this->state = $state;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function run() {
+    $messages = [];
+    $php_sapi = $this->state->get('automatic_updates.php_sapi', PHP_SAPI);
+    if ($php_sapi !== PHP_SAPI) {
+      $messages[] = $this->t('PHP changed from running as "@previous" to "@current". This can lead to inconsistent and misleading results.', ['@previous' => $php_sapi, '@current' => PHP_SAPI]);
+    }
+    return $messages;
+  }
+
+}
---
+++ tests/src/Kernel/ReadinessChecker/PhpSapiTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\Tests\automatic_updates\Kernel\ReadinessChecker;
+
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Tests what happens when PHP SAPI changes from one value to another.
+ *
+ * @group automatic_updates
+ */
+class PhpSapiTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'automatic_updates',
+  ];
+
+  /**
+   * Tests the functionality of supported PHP version readiness checks.
+   */
+  public function testSupportedPhpVersion() {
+    $messages = $this->container->get('automatic_updates.php_sapi')->run();
+    $this->assertEmpty($messages);
+    $messages = $this->container->get('automatic_updates.php_sapi')->run();
+    $this->assertEmpty($messages);
+
+    $this->container->get('state')->set('automatic_updates.php_sapi', 'foo');
+    $messages = $this->container->get('automatic_updates.php_sapi')->run();
+    $this->assertEquals('PHP changed from running as "foo" to "cli". This can lead to inconsistent and misleading results.', $messages[0]);
+  }
+
+}
