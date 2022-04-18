e8e50522071edca5eaaab0e075b4dcdc6c6ab12c
phenaproxima <phenaproxima@205645.no-reply.drupal.org>
2022-04-18 17:26:36 +0000

    Issue #3275508 by phenaproxima: Create a validator to warn if xdebug is on

---
+++ src/Validator/XdebugValidator.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\automatic_updates\Validator;
+
+use Drupal\automatic_updates\Event\ReadinessCheckEvent;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Flags a warning if Xdebug is enabled.
+ */
+class XdebugValidator implements EventSubscriberInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * Flags a warning if Xdebug is enabled.
+   *
+   * @param \Drupal\automatic_updates\Event\ReadinessCheckEvent $event
+   *   The event object.
+   */
+  public function checkForXdebug(ReadinessCheckEvent $event): void {
+    if (extension_loaded('xdebug')) {
+      $event->addWarning([
+        $this->t('Xdebug is enabled, which may cause timeout errors during automatic updates.'),
+      ]);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      ReadinessCheckEvent::class => 'checkForXdebug',
+    ];
+  }
+
+}
