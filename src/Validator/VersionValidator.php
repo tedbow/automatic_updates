41bf2d72a5ebf3b93a4924ae2c8effa77123d2a5
Phéna Proxima <adam@phenaproxima.net>
2022-05-12 11:53:53 -0400

    Fold UpdateReleaseValidator into VersionValidator.

--- src/Validator/VersionValidator.php
+++ src/Validator/VersionValidator.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\automatic_updates\Validator;
 
+use Composer\Semver\Semver;
+use Drupal\automatic_updates\CronUpdater;
 use Drupal\automatic_updates\Event\ReadinessCheckEvent;
 use Drupal\automatic_updates\ProjectInfo;
 use Drupal\automatic_updates\Updater;
@@ -36,6 +38,133 @@ final class VersionValidator implements EventSubscriberInterface {
   }
 
   /**
+   * Checks that the target version of Drupal is valid.
+   *
+   * @param \Drupal\package_manager\Event\StageEvent $event
+   *   The event object.
+   */
+  public function checkTargetVersion(StageEvent $event): void {
+    // Only do these checks for automatic updates.
+    if (!$event->getStage() instanceof Updater) {
+      return;
+    }
+
+    if (!$this->isTargetVersionAcceptable($event)) {
+      return;
+    }
+  }
+
+  /**
+   * Checks if the target version of Drupal is a secure, supported release.
+   *
+   * @param \Drupal\package_manager\Event\StageEvent $event
+   *   The event object.
+   *
+   * @return bool
+   *   TRUE if the target version of Drupal is in the list of secure, supported
+   *   releases; otherwise FALSE.
+   */
+  private function isTargetVersionAcceptable(StageEvent $event): bool {
+    $target_version = $this->getTargetVersion($event);
+    // If the target version isn't in the list of installable releases, then it
+    // isn't secure and supported and we should flag an error.
+    $releases = $this->getAvailableReleases();
+    if (empty($releases) || !array_key_exists($target_version, $releases)) {
+      $message = $this->t('Cannot update Drupal core to @target_version because it is not in the list of installable releases.', [
+        '@target_version' => $target_version,
+      ]);
+      $event->addError([$message]);
+      return FALSE;
+    }
+    return TRUE;
+  }
+
+  /**
+   * Returns the target version of Drupal core.
+   *
+   * @param \Drupal\package_manager\Event\StageEvent $event
+   *   The event object.
+   *
+   * @return string|null
+   *   The target version of Drupal core, or NULL if it could not be determined.
+   */
+  private function getTargetVersion(StageEvent $event): ?string {
+    if ($event instanceof ReadinessCheckEvent) {
+      $package_versions = $event->getPackageVersions();
+    }
+    else {
+      /** @var \Drupal\automatic_updates\Updater $stage */
+      $stage = $event->getStage();
+      $package_versions = $stage->getPackageVersions()['production'];
+    }
+
+    if ($package_versions) {
+      $core_package_name = key($updater->getActiveComposer()->getCorePackages());
+      return $package_versions[$core_package_name];
+    }
+    return $this->getTargetVersionFromAvailableReleases($stage);
+  }
+
+  /**
+   * Returns the target version of Drupal from the list of available releases.
+   *
+   * @param \Drupal\package_manager\Event\StageEvent $event
+   *   The event object.
+   *
+   * @return string|null
+   *   The target version of Drupal core, or NULL if it could not be determined.
+   *
+   * @todo Expand this doc comment to explain how the list of available releases
+   *   is fetched, sorted, and filtered through (i.e., must match the current
+   *   minor). Maybe reference ProjectInfo::getInstallableReleases().
+   */
+  private function getTargetVersionFromAvailableReleases(StageEvent $event): ?string {
+    $installed_version = $this->getInstalledVersion();
+
+    foreach ($this->getAvailableReleases($event) as $possible_release) {
+      $possible_version = $possible_release->getVersion();
+      if (Semver::satisfies($possible_version, "~$installed_version")) {
+        return $possible_version;
+      }
+    }
+    return NULL;
+  }
+
+  /**
+   * Returns the available releases of Drupal core.
+   *
+   * @param \Drupal\package_manager\Event\StageEvent $event
+   *   The event object.
+   *
+   * @return \Drupal\automatic_updates_9_3_shim\ProjectRelease[]
+   *   The available releases of Drupal core, keyed by version number.
+   *
+   * @todo Expand this doc comment to explain what "installable" means (i.e.,
+   *   reference ProjectInfo::getInstallableReleases()), and how the returned
+   *   releases are sorted.
+   */
+  private function getAvailableReleases(StageEvent $event): array {
+    $project_info = new ProjectInfo('drupal');
+    $available_releases = $project_info->getInstallableReleases() ?? [];
+
+    if ($event->getStage() instanceof CronUpdater) {
+      $available_releases = array_reverse($available_releases);
+    }
+    return $available_releases;
+  }
+
+  /**
+   * Returns the currently installed version of Drupal core.
+   *
+   * @return string|null
+   *   The currently installed version of Drupal core, or NULL if it could not
+   *   be determined.
+   */
+  private function getInstalledVersion(): ?string {
+    return (new ProjectInfo('drupal'))->getInstalledVersion();
+  }
+
+  /**
    * Checks if the installed version of Drupal is a dev snapshot.
    *
    * @param \Drupal\package_manager\Event\StageEvent $event
@@ -46,7 +175,7 @@ final class VersionValidator implements EventSubscriberInterface {
    *   FALSE.
    */
   private function isDevSnapshotInstalled(StageEvent $event): bool {
-    $installed_version = (new ProjectInfo('drupal'))->getInstalledVersion();
+    $installed_version = $this->getInstalledVersion();
     $extra = ExtensionVersion::createFromVersionString($installed_version)
       ->getVersionExtra();
 
@@ -64,11 +193,15 @@ final class VersionValidator implements EventSubscriberInterface {
    * {@inheritdoc}
    */
   public static function getSubscribedEvents() {
-    $listeners = ['checkInstalledVersion'];
-
     return [
-      ReadinessCheckEvent::class => $listeners,
-      PreCreateEvent::class => $listeners,
+      ReadinessCheckEvent::class => [
+        ['checkInstalledVersion'],
+        ['checkTargetVersion'],
+      ],
+      PreCreateEvent::class => [
+        ['checkInstalledVersion'],
+        ['checkTargetVersion'],
+      ],
     ];
   }
 
