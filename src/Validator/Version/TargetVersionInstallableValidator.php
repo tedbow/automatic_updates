53f54184ebda33567eabbfeae9906b8dfec737f3
Phéna Proxima <adam@phenaproxima.net>
2022-05-12 15:30:43 -0400

    Fix a thing

--- src/Validator/Version/TargetVersionInstallableValidator.php
+++ src/Validator/Version/TargetVersionInstallableValidator.php
@@ -12,7 +12,7 @@ class TargetVersionInstallableValidator extends VersionValidatorBase {
   protected function doValidation(Updater $updater, string $installed_version, ?string $target_version): array {
     // If the target version isn't in the list of installable releases, then it
     // isn't secure and supported and we should flag an error.
-    $releases = $this->getAvailableReleases($event);
+    $releases = $this->getAvailableReleases($updater);
     if (empty($releases) || !array_key_exists($target_version, $releases)) {
       return [
         $this->t('Cannot update Drupal core to @target_version because it is not in the list of installable releases.'),
