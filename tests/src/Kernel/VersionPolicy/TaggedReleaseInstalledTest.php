bb08495393106c7cba27e6d72438adf1bd797c5b
Phéna Proxima <adam@phenaproxima.net>
2022-05-13 15:13:28 -0400

    Add TaggedReleaseInstalledTest

--- tests/src/Kernel/VersionPolicy/TaggedReleaseInstalledTest.php
+++ tests/src/Kernel/VersionPolicy/TaggedReleaseInstalledTest.php
@@ -2,15 +2,15 @@
 
 namespace Drupal\Tests\automatic_updates\Kernel\VersionPolicy;
 
-use Drupal\automatic_updates\Validator\VersionPolicy\StableReleaseInstalled;
+use Drupal\automatic_updates\Validator\VersionPolicy\TaggedReleaseInstalled;
 use Drupal\Tests\automatic_updates\Kernel\AutomaticUpdatesKernelTestBase;
 
 /**
- * @covers \Drupal\automatic_updates\Validator\VersionPolicy\StableReleaseInstalled
+ * @covers \Drupal\automatic_updates\Validator\VersionPolicy\TaggedReleaseInstalled
  *
  * @group automatic_updates
  */
-class StableReleaseInstalledTest extends AutomaticUpdatesKernelTestBase {
+class TaggedReleaseInstalledTest extends AutomaticUpdatesKernelTestBase {
 
   /**
    * {@inheritdoc}
@@ -18,12 +18,12 @@ class StableReleaseInstalledTest extends AutomaticUpdatesKernelTestBase {
   protected static $modules = ['automatic_updates'];
 
   /**
-   * Data provider for ::testStableReleaseInstalled().
+   * Data provider for ::testTaggedReleaseInstalled().
    *
    * @return array[]
    *   Sets of arguments to pass to the test method.
    */
-  public function providerStableReleaseInstalled(): array {
+  public function providerTaggedReleaseInstalled(): array {
     return [
       'stable version installed, attended' => [
         'automatic_updates.updater',
@@ -38,38 +38,48 @@ class StableReleaseInstalledTest extends AutomaticUpdatesKernelTestBase {
       'alpha version installed, attended' => [
         'automatic_updates.updater',
         '9.8.0-alpha3',
-        ['Drupal cannot be automatically updated during cron from its current version, 9.8.0-alpha3, because Automatic Updates only supports updating from stable versions during cron.'],
+        [],
       ],
       'alpha version installed, unattended' => [
         'automatic_updates.cron_updater',
         '9.8.0-alpha3',
-        ['Drupal cannot be automatically updated during cron from its current version, 9.8.0-alpha3, because Automatic Updates only supports updating from stable versions during cron.'],
+        [],
       ],
       'beta version installed, attended' => [
         'automatic_updates.updater',
         '9.8.0-beta7',
-        ['Drupal cannot be automatically updated during cron from its current version, 9.8.0-beta7, because Automatic Updates only supports updating from stable versions during cron.'],
+        [],
       ],
       'beta version installed, unattended' => [
         'automatic_updates.cron_updater',
         '9.8.0-beta7',
-        ['Drupal cannot be automatically updated during cron from its current version, 9.8.0-beta7, because Automatic Updates only supports updating from stable versions during cron.'],
+        [],
       ],
       'release candidate installed, attended' => [
         'automatic_updates.updater',
         '9.8.0-rc2',
-        ['Drupal cannot be automatically updated during cron from its current version, 9.8.0-rc2, because Automatic Updates only supports updating from stable versions during cron.'],
+        [],
       ],
       'release candidate installed, unattended' => [
         'automatic_updates.cron_updater',
         '9.8.0-rc2',
-        ['Drupal cannot be automatically updated during cron from its current version, 9.8.0-rc2, because Automatic Updates only supports updating from stable versions during cron.'],
+        [],
+      ],
+      'dev snapshot installed, attended' => [
+        'automatic_updates.updater',
+        '9.8.0-dev',
+        ['Drupal cannot be automatically updated from the installed version, 9.8.0-dev, because automatic updates from a dev version to any other version are not supported.'],
+      ],
+      'dev snapshot installed, unattended' => [
+        'automatic_updates.cron_updater',
+        '9.8.0-dev',
+        ['Drupal cannot be automatically updated from the installed version, 9.8.0-dev, because automatic updates from a dev version to any other version are not supported.'],
       ],
     ];
   }
 
   /**
-   * Tests that trying to update from a non-stable release raises an error.
+   * Tests that trying to update from a dev snapshot raises an error.
    *
    * @param string $updater
    *   The updater service to use.
@@ -78,11 +88,11 @@ class StableReleaseInstalledTest extends AutomaticUpdatesKernelTestBase {
    * @param string[] $expected_errors
    *   The expected error messages, if any.
    *
-   * @dataProvider providerStableReleaseInstalled
+   * @dataProvider providerTaggedReleaseInstalled
    */
-  public function testStableReleaseInstalled(string $updater, string $installed_version, array $expected_errors): void {
+  public function testTaggedReleaseInstalled(string $updater, string $installed_version, array $expected_errors): void {
     $updater = $this->container->get($updater);
-    $rule = new StableReleaseInstalled();
+    $rule = new TaggedReleaseInstalled();
     $actual_errors = array_map('strval', $rule->validate($updater, $installed_version, '9.8.1'));
     $this->assertSame($expected_errors, $actual_errors);
   }
