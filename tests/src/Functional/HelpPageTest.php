3474fb9ce3d015ea32845c79240d11f8b3172892
Theresa.Grannum <theresa.grannum@3688861.no-reply.drupal.org>
2022-10-05 15:37:07 +0000

    Issue #3313630 by Theresa.Grannum, phenaproxima: Automatic Updates help has the wrong route name and errors

---
+++ tests/src/Functional/HelpPageTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\Tests\automatic_updates\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * @group automatic_updates
+ */
+class HelpPageTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'automatic_updates',
+    'help',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Tests that the help page for Automatic Updates loads correctly.
+   */
+  public function testHelpPage(): void {
+    $user = $this->createUser([
+      'access administration pages',
+    ]);
+    $this->drupalLogin($user);
+    $this->drupalGet('/admin/help/automatic_updates');
+
+    $assert_session = $this->assertSession();
+    $assert_session->statusCodeEquals(200);
+    $assert_session->pageTextContains('Automatic Updates');
+  }
+
+}
