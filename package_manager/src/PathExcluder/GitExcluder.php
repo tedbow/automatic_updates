2b5ef5694be6e50f5e0228219eb13482d5a33807
Kunal Sachdev <57170-kunal.sachdev@users.noreply.drupalcode.org>
2023-01-26 16:13:56 +0000

    Issue #3315834 by omkar.podey, kunal.sachdev, tedbow, Wim Leers, phenaproxima, daisyleroy: GitExcluder should not ignore .git directories that belong to packages installed by Composer

--- package_manager/src/PathExcluder/GitExcluder.php
+++ package_manager/src/PathExcluder/GitExcluder.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace Drupal\package_manager\PathExcluder;
 
+use Drupal\Core\File\FileSystemInterface;
 use Drupal\package_manager\Event\CollectIgnoredPathsEvent;
 use Drupal\package_manager\PathLocator;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -22,13 +23,23 @@ final class GitExcluder implements EventSubscriberInterface {
   use PathExclusionsTrait;
 
   /**
+   * The file system service.
+   *
+   * @var \Drupal\Core\File\FileSystemInterface
+   */
+  protected $fileSystem;
+
+  /**
    * Constructs a GitExcluder object.
    *
    * @param \Drupal\package_manager\PathLocator $path_locator
    *   The path locator service.
+   * @param \Drupal\Core\File\FileSystemInterface $file_system
+   *   The file system service.
    */
-  public function __construct(PathLocator $path_locator) {
+  public function __construct(PathLocator $path_locator, FileSystemInterface $file_system) {
     $this->pathLocator = $path_locator;
+    $this->fileSystem = $file_system;
   }
 
   /**
@@ -47,9 +58,9 @@ final class GitExcluder implements EventSubscriberInterface {
    *   The event object.
    */
   public function excludeGitDirectories(CollectIgnoredPathsEvent $event): void {
-    // Find all .git directories in the project and exclude them. We cannot do
-    // this with FileSystemInterface::scanDirectory() because it unconditionally
-    // excludes anything starting with a dot.
+    // Find all .git directories in the project. We cannot do this with
+    // FileSystemInterface::scanDirectory() because it unconditionally excludes
+    // anything starting with a dot.
     $finder = Finder::create()
       ->in($this->pathLocator->getProjectRoot())
       ->directories()
@@ -57,11 +68,26 @@ final class GitExcluder implements EventSubscriberInterface {
       ->ignoreVCS(FALSE)
       ->ignoreDotFiles(FALSE);
 
-    $paths = [];
-    foreach ($finder as $git_directory) {
-      $paths[] = $git_directory->getPathname();
+    $paths_to_exclude = [];
+
+    $installed_paths = [];
+    // Collect the paths of every installed package.
+    $installed_packages = $event->getStage()->getActiveComposer()->getInstalledPackagesData();
+    foreach ($installed_packages as $package_data) {
+      if (array_key_exists('install_path', $package_data) && !empty($package_data['install_path'])) {
+        $installed_paths[] = $this->fileSystem->realpath($package_data['install_path']);
+      }
     }
-    $this->excludeInProjectRoot($event, $paths);
+    foreach ($finder as $git_directory) {
+      // Don't exclude any `.git` directory that is directly under an installed
+      // package's path, since it means Composer probably installed that package
+      // from source and therefore needs the `.git` directory in order to update
+      // the package.
+      if (!in_array(dirname((string) $git_directory), $installed_paths, TRUE)) {
+        $paths_to_exclude[] = $git_directory->getPathname();
+      }
+    }
+    $this->excludeInProjectRoot($event, $paths_to_exclude);
   }
 
 }
--- package_manager/tests/src/Kernel/PathExcluder/GitExcluderTest.php
+++ package_manager/tests/src/Kernel/PathExcluder/GitExcluderTest.php
@@ -4,7 +4,12 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\package_manager\Kernel\PathExcluder;
 
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\File\FileSystemInterface;
+use Drupal\Core\Serialization\Yaml;
+use Drupal\fixture_manipulator\ActiveFixtureManipulator;
 use Drupal\Tests\package_manager\Kernel\PackageManagerKernelTestBase;
+use Symfony\Component\Filesystem\Filesystem;
 
 /**
  * @covers \Drupal\package_manager\PathExcluder\GitExcluder
@@ -12,6 +17,13 @@ use Drupal\Tests\package_manager\Kernel\PackageManagerKernelTestBase;
  * @internal
  */
 class GitExcluderTest extends PackageManagerKernelTestBase {
+
+  /**
+   * The mocked file system service.
+   *
+   * @var \Drupal\Core\File\FileSystemInterface|\Prophecy\Prophecy\ObjectProphecy
+   */
+  private $fileSystem;
 
   /**
    * {@inheritdoc}
@@ -22,41 +34,100 @@ class GitExcluderTest extends PackageManagerKernelTestBase {
     // this validator will complain because they don't differ at all.
     $this->disableValidators[] = 'package_manager.validator.lock_file';
     parent::setUp();
+    $path_locator = $this->container->get('package_manager.path_locator');
+    (new ActiveFixtureManipulator())
+      ->addPackage([
+        'name' => 'foo/package_known_to_composer_removed_later',
+        'type' => 'drupal-module',
+        'version' => '1.0.0',
+        'install_path' => "../../modules/module_known_to_composer_removed_later",
+      ])
+      ->addProjectAtPath("modules/module_not_known_to_composer_in_active")
+      ->addDotGitFolder($path_locator->getProjectRoot() . "/modules/module_not_known_to_composer_in_active")
+      ->addDotGitFolder($path_locator->getProjectRoot() . "/modules/module_known_to_composer_removed_later")
+      ->commitChanges();
   }
 
   /**
-   * Tests that Git directories are excluded from stage operations.
+   * {@inheritdoc}
    */
-  public function testGitDirectoriesExcluded(): void {
-    // In this test, we want to perform the actual stage operations so that we
-    // can be sure that files are staged as expected.
-    $this->setSetting('package_manager_bypass_composer_stager', FALSE);
+  public function register(ContainerBuilder $container) {
+    parent::register($container);
+
+    $this->fileSystem = $this->prophesize(FileSystemInterface::class);
+
+    $container->getDefinition('package_manager.git_excluder')
+      ->setArgument('$file_system', $this->fileSystem->reveal());
+  }
+
+  /**
+   * Tests that Git directories are excluded from stage during PreCreate.
+   */
+  public function testGitDirectoriesExcludedActive(): void {
     // Ensure we have an up-to-date container.
     $this->container = $this->container->get('kernel')->rebuildContainer();
 
-    $active_dir = $this->container->get('package_manager.path_locator')
-      ->getProjectRoot();
+    $stage = $this->createStage();
+    $stage->create();
+    /** @var \Drupal\package_manager_bypass\BypassedStagerServiceBase $beginner */
+    $beginner = $this->container->get('package_manager.beginner');
+    $beginner_args = $beginner->getInvocationArguments();
+    $excluded_paths = [
+      '.git',
+      'modules/module_not_known_to_composer_in_active/.git',
+      'modules/example/.git',
+    ];
+    foreach ($excluded_paths as $excluded_path) {
+      $this->assertContains($excluded_path, $beginner_args[0][2]->getAll());
+    }
+  }
+
+  /**
+   * Tests that Git directories are excluded from active during PreApply.
+   */
+  public function testGitDirectoriesExcludedStage(): void {
+    // Ensure we have an up-to-date container.
+    $this->container = $this->container->get('kernel')->rebuildContainer();
+
+    $this->getStageFixtureManipulator()
+      ->removePackage('foo/package_known_to_composer_removed_later');
 
     $stage = $this->createStage();
     $stage->create();
     $stage_dir = $stage->getStageDirectory();
 
-    $ignored = [
-      '.git/ignore.txt',
-      'modules/example/.git/ignore.txt',
-    ];
-    foreach ($ignored as $path) {
-      $this->assertFileExists("$active_dir/$path");
-      $this->assertFileDoesNotExist("$stage_dir/$path");
-    }
-    // Files that start with .git, but aren't actually .git, should be staged.
-    $this->assertFileExists("$stage_dir/.gitignore");
+    // Adding a module with .git in stage which is unknown to composer, we
+    // expect it to not be copied to the active directory.
+    $path = "$stage_dir/modules/unknown_to_composer_in_stage";
+    $fs = new Filesystem();
+    $fs->mkdir("$path/.git");
+    file_put_contents(
+      "$path/unknown_to_composer.info.yml",
+      Yaml::encode([
+        'name' => 'Unknown to composer in stage',
+        'type' => 'module',
+        'core_version_requirement' => '^9.3 || ^10',
+      ])
+    );
+    file_put_contents("$path/.git/ignored.txt", 'Phoenix!');
 
     $stage->apply();
-    // The ignored files should still be in the active directory.
-    foreach ($ignored as $path) {
-      $this->assertFileExists("$active_dir/$path");
+    /** @var \Drupal\package_manager_bypass\BypassedStagerServiceBase $committer */
+    $committer = $this->container->get('package_manager.committer');
+    $committer_args = $committer->getInvocationArguments();
+    $excluded_paths = [
+      '.git',
+      'modules/module_not_known_to_composer_in_active/.git',
+      'modules/example/.git',
+    ];
+    // We are missing "modules/unknown_to_composer_in_stage/.git" in excluded
+    // paths because there is no validation for it as it is assumed about any
+    // new .git folder in stage directory that either composer is aware of it or
+    // the developer knows what they are doing.
+    foreach ($excluded_paths as $excluded_path) {
+      $this->assertContains($excluded_path, $committer_args[0][2]->getAll());
     }
+    $this->assertNotContains('modules/unknown_to_composer_in_stage/.git', $committer_args[0][2]->getAll());
   }
 
 }
