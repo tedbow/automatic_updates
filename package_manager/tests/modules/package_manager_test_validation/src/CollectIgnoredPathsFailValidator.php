67676ee8ed7d66a5ad188a4c9f62eb4279d67f62
Adam G-H <32250-phenaproxima@users.noreply.drupalcode.org>
2023-03-06 12:51:05 +0000

    Issue #3345766 by phenaproxima: CollectIgnoredPathsFailValidator should use ComposerInspector instead of ComposerUtility

--- package_manager/tests/modules/package_manager_test_validation/src/CollectIgnoredPathsFailValidator.php
+++ package_manager/tests/modules/package_manager_test_validation/src/CollectIgnoredPathsFailValidator.php
@@ -4,13 +4,26 @@ declare(strict_types = 1);
 
 namespace Drupal\package_manager_test_validation;
 
+use Drupal\package_manager\ComposerInspector;
 use Drupal\package_manager\Event\CollectIgnoredPathsEvent;
+use Drupal\package_manager\PathLocator;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
  * Allows to test an excluder which fails on CollectIgnoredPathsEvent.
  */
 class CollectIgnoredPathsFailValidator implements EventSubscriberInterface {
+
+  /**
+   * Constructs a CollectIgnoredPathsFailValidator object.
+   *
+   * @param \Drupal\package_manager\ComposerInspector $composerInspector
+   *   The Composer inspector service.
+   * @param \Drupal\package_manager\PathLocator $pathLocator
+   *   The path locator service.
+   */
+  public function __construct(private ComposerInspector $composerInspector, private PathLocator $pathLocator) {
+  }
 
   /**
    * {@inheritdoc}
@@ -24,8 +37,8 @@ class CollectIgnoredPathsFailValidator implements EventSubscriberInterface {
   /**
    * Fails when composer.json is deleted to simulate failure on excluders.
    */
-  public function callToComposer(CollectIgnoredPathsEvent $event) {
-    $event->stage->getActiveComposer();
+  public function callToComposer() {
+    $this->composerInspector->validate($this->pathLocator->getProjectRoot());
   }
 
 }
