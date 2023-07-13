bb38f049fae4dfcfa856a1d175f798396a953fc7
Ted Bowman <ted+git@tedbow.com>
2023-07-13 08:26:16 -0400

    update getCommandPath for test

--- tests/modules/automatic_updates_test/src/TestCronUpdateRunner.php
+++ tests/modules/automatic_updates_test/src/TestCronUpdateRunner.php
@@ -4,7 +4,6 @@ namespace Drupal\automatic_updates_test;
 
 use Composer\Autoload\ClassLoader;
 use Drupal\automatic_updates\CronUpdateRunner;
-use function PHPUnit\Framework\assertCount;
 
 class TestCronUpdateRunner extends CronUpdateRunner {
 
@@ -12,10 +11,32 @@ class TestCronUpdateRunner extends CronUpdateRunner {
    * {@inheritdoc}
    */
   protected function getCommandPath(): string {
-    // Return the real path of Drush.
+    // There may be multiple class loaders at work.
+    // ClassLoader::getRegisteredLoaders() keeps track of them all, indexed by
+    // the path of the vendor directory they load classes from.
     $loaders = ClassLoader::getRegisteredLoaders();
-    assertCount(1, $loaders);
-    $vendor_path = array_keys($loaders)[0];
+
+    // If there's only one class loader, we don't need to search for the right
+    // one.
+    if (count($loaders) === 1) {
+      $vendor_path = key($loaders);
+    }
+    else {
+      // To determine which class loader is the one for Drupal's vendor directory,
+      // look for the loader whose vendor path starts the same way as the path to
+      // this file.
+      foreach (array_keys($loaders) as $path) {
+        if (str_starts_with(__FILE__, dirname($path))) {
+          $vendor_path = $path;
+        }
+      }
+    }
+    if (!isset($vendor_path)) {
+      // If we couldn't find a match, assume that the first registered class
+      // loader is the one we want.
+      $vendor_path = key($loaders);
+    }
+
     return $vendor_path . '/drush/drush/drush';
   }
 
