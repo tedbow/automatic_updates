393af499438ea1466a97e19ff0e46a99fc02d6c1
Ted Bowman <ted+git@tedbow.com>
2023-10-12 09:52:05 -0400

    fix from 3393633

--- src/ConsoleUpdateStage.php
+++ src/ConsoleUpdateStage.php
@@ -262,7 +262,11 @@ class ConsoleUpdateStage extends UpdateStage {
    *   The ID of the current stage.
    */
   protected function triggerPostApply(string $stage_id): void {
-    $arguments = "post-apply \"$stage_id\"";
+    // The stage ID needs to be quoted in order to prevent it from being parsed
+    // as a command-line option if it begins with -, which is a possibility
+    // because we use \Drupal\Component\Utility\Crypt::randomBytesBase64() to
+    // generate the stage ID, and the string it returns might begin with -.
+    $arguments = sprintf('post-apply "%s"', $stage_id);
     if ($this->isFromWeb) {
       $arguments .= ' --is-from-web';
     }
